6cafbc5cd21fe2fba330824d7d3fd6cf55bcfbd5
Satoshi Tanimoto <stanimoto@thearenagroup.net>
2024-03-13 10:04:13 -0700

    test

--- openai_embeddings.py
+++ openai_embeddings.py
@@ -4,6 +4,14 @@ from neo4j import GraphDatabase, Result
 import pandas as pd
 from openai.error import APIError
 from time import sleep
+from bs4 import BeautifulSoup
+
+
+def remove_html_markup(html_text):
+    soup = BeautifulSoup(html_text, 'html.parser')
+    plain_text = soup.get_text()
+    return plain_text
+
 
 def generate_embeddings(file_name, limit=None):
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -15,29 +23,30 @@ def generate_embeddings(file_name, limit=None):
 
     driver.verify_connectivity()
 
-    query = """MATCH (m:Movie) WHERE m.plot IS NOT NULL
-    RETURN m.movieId AS movieId, m.title AS title, m.plot AS plot"""
+    query = """MATCH (a:Article) WHERE a.body IS NOT NULL
+    RETURN a.id AS id, a.title AS title, a.body AS body"""
 
     if limit is not None:
         query += f" LIMIT {limit}"
 
-    movies = driver.execute_query(
+    articles = driver.execute_query(
         query,
         result_transformer_=Result.to_df
     )
 
-    print(len(movies))
-    
+    print(len(articles))
+
     embeddings = []
 
-    for _, n in movies.iterrows():
-        
+    for _, n in articles.iterrows():
+        body = remove_html_markup(n['body'])
+
         successful_call = False
         while not successful_call:
             try:
                 res = openai.Embedding.create(
                     model="text-embedding-ada-002",
-                    input=f"{n['title']}: {n['plot']}",
+                    input=f"{n['title']}: {body}",
                     encoding_format="float"
                 )
                 successful_call = True
@@ -48,11 +57,11 @@ def generate_embeddings(file_name, limit=None):
 
         print(n['title'])
 
-        embeddings.append({"movieId": n['movieId'], "embedding": res['data'][0]['embedding']})
+        embeddings.append({"id": n['id'], "embedding": res['data'][0]['embedding']})
 
     embedding_df = pd.DataFrame(embeddings)
     embedding_df.head()
     embedding_df.to_csv(file_name, index=False)
 
-generate_embeddings('data\openai-embeddings.csv',limit=1000)
-generate_embeddings('data\openai-embeddings-full.csv',limit=None)
+#generate_embeddings('openai-embeddings.csv',limit=1)
+generate_embeddings('openai-embeddings-full.csv',limit=None)
